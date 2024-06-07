9d873fdeeed33fd55fa5103c1dcbf45b9e6d83f6
Luka <luka.luka.luka93@gmail.com>
2024-06-07 16:34:34 -0700

    addNote method created, and set onClick

--- src/App.js
+++ src/App.js
@@ -4,32 +4,29 @@ import NotesList from "./NotesList.js";
 
 class App extends Component {
     state = {
-        notes: [
-            {
-        id: 0,
-        title: "eat",
-        description: "reese peanut butter cups",
-        doesMatchSearch: true
-      },
-      {
-        id: 1,
-        title: "sleep",
-        description: "eight hours",
-        doesMatchSearch: true
-      },
-      {
-        id: 2,
-        title: "code",
-        description: "build an awesome ui",
-        doesMatchSearch: true
-      }
-        ],
+        notes: [{
+         id: Date.now(),
+         title: "",
+         description: "",
+         doesMatchSearch: true,
+         }],
     searchText: "Search for me"
+    }
+
+    addNote = () => {
+        const newNote = {
+         id: Date.now(),
+         title: "",
+         description: "",
+         doesMatchSearch: true,
+        };
+        const newNotes = [newNote, ...this.state.notes];
+        this.setState({notes: newNotes});
     }
     render(){
         return (
          <div>
-          <Header searchText={this.state.searchText}/>
+          <Header searchText={this.state.searchText} addNote={this.addNote}/>
            <NotesList notes={this.state.notes} />
          </div>
         )
