3beae578222fb67640007f6aedb34808be925043
Luka <luka.luka.luka93@gmail.com>
2024-06-10 19:27:23 -0700

    added comment in a note module

--- src/Note.js
+++ src/Note.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-
+/* This presentational component renders a single
+list item element with two text input fields,
+one for the note's title and one for the note's
+description. When the user types in these fields,
+functions that are passed to this component
+as props are used to update the state of the
+corresponding note object in the state of the
+App component  */
 const Note = props => { 
     const updateTitle = e =>{
         const updatedTitle = e.target.value;
