0d3b6b5299c6ade8270303946c9910b20cac098f
Luka <luka.luka.luka93@gmail.com>
2024-06-10 18:49:04 -0700

    adding updated on change title and description

--- src/Note.js
+++ src/Note.js
@@ -7,11 +7,13 @@ const Note = props => (
             className="note__title" 
             type="text" placeholder="Title"
             value={props.note.title}
+            onChange={updateTitle}
              />
         <textarea 
             className="note__description" 
             placeholder="Description..."
-            value={props.note.description} />
+            value={props.note.description}
+            onChange={updatedDescription} />
         <span className="note__delete">X</span>
       </li>
 )
